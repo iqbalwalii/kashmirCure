db4c73735f77468c5fd21988c58ee101d06f8e81
iqbalwalii <iqbalwali786@gmail.com>
2022-02-05 14:22:02 +0530

    resetted the background and removed unused dependencies

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,79 +1,67 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import {
-	PeopleFill,
-	PhoneFill,
-	BriefcaseFill,
-	JournalMedical,
-	PlusCircleFill,
-} from 'react-bootstrap-icons';
+  PeopleFill,
+  PhoneFill,
+  BriefcaseFill,
+  JournalMedical,
+  PlusCircleFill,
+} from "react-bootstrap-icons";
 // import { Dash } from "react-bootstrap-icons";
-import Dash from '../styles/Dashboard.module.css';
-import Appointment from './Appointments';
-import LineChart from './LineChart';
-import BarChart from './BarChart';
-import PriceChart from './PriceChart';
-import PieChart from './PieChart';
+import Dash from "../styles/Dashboard.module.css";
+import Appointment from "./Appointments";
+import LineChart from "./LineChart";
+import BarChart from "./BarChart";
+import PriceChart from "./PriceChart";
+import PieChart from "./PieChart";
 const Dashboard = () => {
-	return (
-		<Container>
-			{/* Counters Start */}
+  return (
+    <Container>
+      {/* Counters Start */}
 
-			<Row>
-				<Col className={Dash.counters} xs={12}>
-					<div className={Dash.counter}>
-						<div
-							className={Dash.counterIcon}
-							style={{ background: '#FF6270' }}
-						>
-							<PeopleFill size={28} />
-						</div>
-						<div className={Dash.details}>
-							<h6>Total Patients</h6>
-							<h5 className={Dash.counterNumber}>1,000</h5>
-						</div>
-					</div>
-					{/* number two */}
-					<div className={Dash.counter}>
-						<div
-							className={Dash.counterIcon}
-							style={{ background: '#FE764B' }}
-						>
-							<PlusCircleFill size={28} />
-						</div>
-						<div className={Dash.details}>
-							<h6>Total Doctors</h6>
-							<h5 className={Dash.counterNumber}>1,000</h5>
-						</div>
-					</div>
-					{/* number three */}
-					<div className={Dash.counter}>
-						<div
-							className={Dash.counterIcon}
-							style={{ background: '#FFDE1f' }}
-						>
-							<BriefcaseFill size={28} />
-						</div>
-						<div className={Dash.details}>
-							<h6>Total Appointments</h6>
-							<h5 className={Dash.counterNumber}>1,000</h5>
-						</div>
-					</div>
-					{/* number four    */}
-					<div className={Dash.counter}>
-						<div
-							className={Dash.counterIcon}
-							style={{ background: '#70CF98' }}
-						>
-							<JournalMedical size={28} />
-						</div>
-						<div className={Dash.details}>
-							<h6>Total Blogs</h6>
-							<h5 className={Dash.counterNumber}>1,000</h5>
-						</div>
-					</div>
-					{/* number five */}
-					{/* <div className={Dash.counter}>
+      <Row>
+        <Col className={Dash.counters} xs={12}>
+          <div className={Dash.counter}>
+            <div className={Dash.counterIcon} style={{ background: "#FF6270" }}>
+              <PeopleFill size={28} />
+            </div>
+            <div className={Dash.details}>
+              <h6>Total Patients</h6>
+              <h5 className={Dash.counterNumber}>1,000</h5>
+            </div>
+          </div>
+          {/* number two */}
+          <div className={Dash.counter}>
+            <div className={Dash.counterIcon} style={{ background: "#FE764B" }}>
+              <PlusCircleFill size={28} />
+            </div>
+            <div className={Dash.details}>
+              <h6>Total Doctors</h6>
+              <h5 className={Dash.counterNumber}>1,000</h5>
+            </div>
+          </div>
+          {/* number three */}
+          <div className={Dash.counter}>
+            <div className={Dash.counterIcon} style={{ background: "#FFDE1f" }}>
+              <BriefcaseFill size={28} />
+            </div>
+            <div className={Dash.details}>
+              <h6>Total Appointments</h6>
+              <h5 className={Dash.counterNumber}>1,000</h5>
+            </div>
+          </div>
+          {/* number four    */}
+          <div className={Dash.counter}>
+            <div className={Dash.counterIcon} style={{ background: "#70CF98" }}>
+              <JournalMedical size={28} />
+            </div>
+            <div className={Dash.details}>
+              <h6>Total Blogs</h6>
+              <h5 className={Dash.counterNumber}>1,000</h5>
+            </div>
+          </div>
+          {/* number five */}
+          {/* <div className={Dash.counter}>
 						<div
 							className={Dash.counterIcon}
 							style={{ background: '#2984FF' }}
@@ -85,27 +73,27 @@ const Dashboard = () => {
 							<h5 className={Dash.counterNumber}>1,000</h5>
 						</div>
 					</div> */}
-				</Col>
-			</Row>
-			{/* Counters End*/}
-			<Row>
-				<Col className={Dash.charts} sm={6}>
-					<LineChart />
-				</Col>
-				{/* <Col sm={1}></Col> */}
-				<Col
-					style={{ marginLeft: '2.4rem' }}
-					className={Dash.charts}
-					sm={5}
-				>
-					<BarChart />
-				</Col>
-			</Row>
-			<Row>
-				<Col sm={12}>
-					<Appointment />
-				</Col>
-				{/* <Col
+        </Col>
+      </Row>
+      {/* Counters End*/}
+      <Row>
+        <Col className="appointments mt-3 p-1" sm={6}>
+          <LineChart />
+        </Col>
+        {/* <Col sm={1}></Col> */}
+        <Col
+          style={{ marginLeft: "2.4rem" }}
+          className="appointments mt-3 p-1"
+          sm={5}
+        >
+          <BarChart />
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={12}>
+          <Appointment />
+        </Col>
+        {/* <Col
 					sm={4}
 					style={{
 						transform: 'scale(0.7)',
@@ -114,9 +102,9 @@ const Dashboard = () => {
 				>
 					<PieChart />
 				</Col> */}
-			</Row>
-		</Container>
-	);
+      </Row>
+    </Container>
+  );
 };
 
 export default Dashboard;
