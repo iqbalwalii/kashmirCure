9e14655e58d1111cf99bbafa3e8cdcdc9ea69abe
iqbalwalii <iqbalwali786@gmail.com>
2022-02-17 11:37:24 +0530

    fetched doctors and patients from backend

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
-import axios from "axios";
-export default axios.create({
-  baseURL: "https://kashmircure.herokuapp.com/api/",
+import Axios from "axios";
+const baseURL =
+  process.env.NODE_ENV === "production" ? "/" : "http://localhost:4000";
+const axios = Axios.create({
+  baseURL,
   headers: {
+    Accept: "application/json",
     "Content-Type": "application/json",
   },
 });
-=======
-import Axios from 'axios';
-const baseURL =
-	process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:4000';
-const axios = Axios.create({
-	baseURL,
-	headers: {
-		Accept: 'application/json',
-		'Content-Type': 'application/json',
-	},
-});
 
 export default axios;
->>>>>>> c9bd8bc3a307e8c59341433c225126ab4941e44b
