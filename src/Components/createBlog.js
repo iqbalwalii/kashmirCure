7492d5b573b1d0493fd302d6fc708447e97cbd6a
stupidlymoron <officialirfanafzal@gmail.com>
2022-04-11 16:18:40 +0530

    added post function

--- src/Components/createBlog.js
+++ src/Components/createBlog.js
@@ -1,98 +1,137 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
 import { connect } from "react-redux";
+import axios from "../utils/axios";
+import Axios from "axios";
 class App extends React.Component {
-  state = {
-    title: "",
-    description: [""],
-    author_id: this.props?.user?.user?._id,
-  };
-  handleChange = (e) => {
-    e.persist();
+	state = {
+		title: "",
+		description: [""],
+		author_id: this.props?.user?.user?._id,
+	};
+	handleChange = (e) => {
+		e.persist();
 
-    const newInputs = [...this.state.description];
+		const newInputs = [...this.state.description];
 
-    newInputs[e.target.id] = e.target.value;
+		newInputs[e.target.id] = e.target.value;
 
-    if (e.target.value && newInputs.every((input) => input.length)) {
-      newInputs.push("");
-    }
+		if (e.target.value && newInputs.every((input) => input.length)) {
+			newInputs.push("");
+		}
 
-    this.setState(() => ({
-      description: newInputs,
-    }));
-  };
-  onSubmitHandler = (e) => {
-    e.preventDefault();
-    console.log(this.state);
-    // this.state.description.pop();
-    // createPost(this.state).then((res) => {
-    //   this.props.dispatch({
-    //     type: "SET_ACTIVE_TAB",
-    //     payload: "dashboard",
-    //   });
-    // });
-  };
+		this.setState(() => ({
+			description: newInputs,
+		}));
+	};
+	onSubmitHandler = (e) => {
+		e.preventDefault();
+		const FORMDATA = new FormData();
+		FORMDATA.append("adImage", this.state.bannerImage);
+		try {
+			Axios.post("http://localhost:4000/api/posts", {
+				author_id: this.state.author_id,
+				title: this.state.title,
+				description: this.state.description.filter(Boolean),
+			})
+				.then((res) => res)
+				.then((result) => {
+					const { data } = result;
+					if (data.status === "success") {
+						Axios.put(
+							`http://localhost:4000/api/blog/upload/image/${data.data.post._id}`,
+							FORMDATA,
+							{
+								headers: {
+									"Content-Type": "multipart/form-data",
+								},
+							}
+						)
+							.then((res) => res)
+							.then((result) => {
+								console.log("image upload respinse", result);
+							})
+							.catch((err) =>
+								console.log("image upload err", err)
+							);
+					}
+				})
+				.catch((err) => console.log("err", err));
+		} catch (error) {
+			console.log("post catch error");
+		}
+	};
 
-  render() {
-    const { description } = this.state;
+	render() {
+		const { description } = this.state;
 
-    return (
-      <div>
-        <Form onChange={this.handleChange} onSubmit={(e) => e.preventDefault()}>
-          <Form.Group controlId="title">
-            <Form.Label>Title</Form.Label>
-            <Form.Control
-              placeholder="Title of the blog"
-              onChange={(e) => this.setState({ title: e.target.value })}
-            />
-          </Form.Group>
-          <Form.Group controlId="link" className="mt-2">
-            <Form.Label>Image</Form.Label>
-            <Form.Control
-              type="file"
-              onChange={(e) => this.setState({ bannerImage: e.target.value })}
-            />
-          </Form.Group>
-          <Form.Group controlId="description" className="mt-2">
-            <Form.Label>Description</Form.Label>
-            {description.map((input, i) => {
-              return (
-                <textarea
-                  className="textarea"
-                  placeholder="Enter the description "
-                  key={i}
-                  id={i}
-                  value={input}
-                  onChange={(e) =>
-                    this.setState({ description: e.target.value })
-                  }
-                />
-              );
-            })}
-          </Form.Group>
-          <div className="d-grid">
-            <Button variant="dark" onClick={this.onSubmitHandler}>
-              Submit
-            </Button>
+		return (
+			<div>
+				<Form
+					onChange={this.handleChange}
+					onSubmit={(e) => e.preventDefault()}
+				>
+					<Form.Group controlId="title">
+						<Form.Label>Title</Form.Label>
+						<Form.Control
+							placeholder="Title of the blog"
+							onChange={(e) =>
+								this.setState({ title: e.target.value })
+							}
+						/>
+					</Form.Group>
+					<Form.Group controlId="link" className="mt-2">
+						<Form.Label>Image</Form.Label>
+						<Form.Control
+							type="file"
+							onChange={(e) => {
+								this.setState({
+									bannerImage: e.target.files[0],
+								});
+							}}
+						/>
+					</Form.Group>
+					<Form.Group controlId="description" className="mt-2">
+						<Form.Label>Description</Form.Label>
+						{description.map((input, i) => {
+							return (
+								<textarea
+									className="textarea"
+									placeholder="Enter the description "
+									key={i}
+									id={i}
+									value={input}
+									onChange={(e) =>
+										this.setState({
+											description: e.target.value,
+										})
+									}
+								/>
+							);
+						})}
+					</Form.Group>
+					<div className="d-grid">
+						<Button variant="dark" onClick={this.onSubmitHandler}>
+							continue
+						</Button>
 
-            <Button
-              variant="primary"
-              className="mt-1"
-              onClick={() => this.setState({ description: [""] })}
-            >
-              Clear
-            </Button>
-          </div>
-        </Form>
-      </div>
-    );
-  }
+						<Button
+							variant="primary"
+							className="mt-1"
+							onClick={() => this.setState({ description: [""] })}
+						>
+							Clear
+						</Button>
+					</div>
+				</Form>
+			</div>
+		);
+	}
 }
 const mapStateToProps = (state) => {
-  return {
-    user: state.user,
-    tab: state.tab,
-  };
+	return {
+		user: state.user,
+		tab: state.tab,
+	};
 };
 export default connect(mapStateToProps)(App);
