e41a44f9c23b025bc510427e9e2dbc083734b8ec
iqbalwalii <iqbalwali786@gmail.com>
2022-04-12 18:55:53 +0530

    graphs

--- src/Components/LineChart.js
+++ src/Components/LineChart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import {
   Chart as ChartJS,
@@ -20,56 +20,57 @@ ChartJS.register(
   Tooltip,
   Legend
 );
-const labels = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "June",
-  "July",
-  "Aug",
-  "Sept",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-
-export const options = {
-  responsive: +true,
-  plugins: {
-    legend: {
-      position: "top",
-    },
-    title: {
-      display: false,
-      text: "Kiadah",
-    },
-  },
-};
-const nums = [0, 10, 20, 50, 60, 70, 80, 30, 40, 90, 100, 120];
-const nums2 = [0, 10, 10, 20, 90, 120, 40, 30, 120, 90, 100, 120];
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "Doctors",
-      data: labels.map((label, index) => nums[index]),
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "Patients",
-      data: labels.map((label, index) => nums2[index]),
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-  ],
-};
 const LineChart = (props) => {
-  const dashboard = props.dashboard;
+  const { dashboard } = props;
+  let docs = new Array(12).fill(0);
+  let patients = new Array(12).fill(0);
+  dashboard?.monthly_doctors.map((doc) => (docs[doc._id - 1] = doc.total));
+  dashboard?.monthly_patients.map((pat) => (patients[pat._id - 1] = pat.total));
+  console.log(docs, patients);
+  const labels = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Doctors",
+        data: labels?.map((label, index) => docs[index]),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Patients",
+        data: labels?.map((label, index) => patients[index]),
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+  const options = {
+    responsive: +true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: false,
+        text: "Kiadah",
+      },
+    },
+  };
 
-  console.log(dashboard);
   return (
     <>
       <Line options={options} data={data} />
