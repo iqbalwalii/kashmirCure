49662c82f805a47506a7ae8be88044939a5d588f
stupidlymoron <officialirfanafzal@gmail.com>
2022-04-16 12:39:48 +0530

    aded cancel details

--- src/Components/Appointments.js
+++ src/Components/Appointments.js
@@ -3,43 +3,70 @@ import { Table } from "react-bootstrap";
 import { connect } from "react-redux";
 import { getAppointments } from "../services/AppointmentService";
 const Appointments = (props) => {
-  const { appointments } = props;
+	const { appointments } = props;
 
-  return (
-    <>
-      <div className="appointments_main">
-        <h6>Recent Appointments</h6>
-        <Table striped hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>Date</th>
-              <th>Age</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {appointments?.slice(0, 5)?.map((patient, index) => {
-              return (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>{patient?.name}</td>
-                  <td>{patient?.start_time?.slice(0, 10)}</td>
-                  <td>{patient?.age}</td>
-                  <td>{patient?.appointment_status}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="appointments_main">
+				<h6>Recent Appointments</h6>
+				<Table striped hover>
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Name</th>
+							<th>Date</th>
+							<th>Age</th>
+							<th>Status</th>
+						</tr>
+					</thead>
+					<tbody>
+						{appointments
+							?.slice(0, 5)
+							?.map((appointment, index) => {
+								return (
+									<tr key={index}>
+										<td>{index + 1}</td>
+										<td>{appointment?.name}</td>
+										<td>
+											{appointment?.start_time?.slice(
+												0,
+												10
+											)}
+										</td>
+										<td>{appointment?.age}</td>
+										<td>
+											{appointment?.appointment_status}
+											{appointment.appointment_status ===
+											"Cancel" ? (
+												<div>
+													<span>
+														{
+															appointment
+																.cancel_details
+																?.cancel_reason
+														}
+													</span>
+													<br />
+													<span>
+														appointment
+														.cancel_details
+														?.cancel_date
+													</span>
+												</div>
+											) : null}
+										</td>
+									</tr>
+								);
+							})}
+					</tbody>
+				</Table>
+			</div>
+		</>
+	);
 };
 const mapStateToProps = (state) => {
-  return {
-    appointments: state.appointments,
-  };
+	return {
+		appointments: state.appointments,
+	};
 };
 export default connect(mapStateToProps)(Appointments);
