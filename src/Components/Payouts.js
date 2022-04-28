c5acb69d95d038e19c5d7cd647170fbfc989915d
iqbalwalii <iqbalwali786@gmail.com>
2022-04-28 21:16:39 +0530

    payouts

--- src/Components/Payouts.js
+++ src/Components/Payouts.js
@@ -1,12 +1,16 @@
-import React, { useEffect } from "react";
-import { Table, Row, Col, Button, Form, InputGroup } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Table, Row, Col, Button, Pagination } from "react-bootstrap";
 import { getPayouts } from "../services/PayoutService";
 import { connect } from "react-redux";
 import { useForm } from "react-hook-form";
 import Link from "next/link";
 import { useRouter } from "next/router";
 const Payouts = (props) => {
+  const { payouts } = props;
   const router = useRouter();
+  let [active, setActive] = useState(1);
+  const pages = Math.ceil(payouts?.total_appointments / 10);
+  let items = [];
   useEffect(() => {
     getPayouts().then((res) => {
       console.log("payouts", res);
@@ -16,8 +20,17 @@ const Payouts = (props) => {
       });
     });
   }, []);
-
-  const { payouts } = props;
+  for (let number = 1; number <= pages; number++) {
+    items.push(
+      <Pagination.Item
+        key={number}
+        active={number === active}
+        onClick={() => handleRequest(number)}
+      >
+        {number}
+      </Pagination.Item>
+    );
+  }
   return (
     <div className="appointments">
       <Row>
@@ -52,8 +65,8 @@ const Payouts = (props) => {
                 <td>{payout?.mode}</td>
                 <td>
                   {payout?.notes?.id && (
-                    <Link href={`/doctor/${payout?.notes?.id}`}>
-                      <Button>View Doctor</Button>
+                    <Link href={`/doctor/${payout?.notes?.id}`} va>
+                      <Button size="sm">View</Button>
                     </Link>
                   )}
                 </td>
@@ -67,11 +80,43 @@ const Payouts = (props) => {
         </tbody>
       </Table>
 
-      {/* <Row>
+      <Row>
         <Col md={{ span: 2, offset: 5 }}>
-          <Pagination size="sm">{items}</Pagination>
+          {active > 1 && (
+            <Button
+              variant="dark"
+              className="mx-2"
+              onClick={() => {
+                getPayouts(active - 1).then((res) => {
+                  console.log("payouts", res);
+                  props.dispatch({
+                    type: "GET_PAYOUTS",
+                    payload: res.data.payouts,
+                  });
+                });
+              }}
+            >
+              Prev
+            </Button>
+          )}
+          {
+            <Button
+              variant="dark"
+              onClick={() => {
+                getPayouts(active + 1).then((res) => {
+                  console.log("payouts", res);
+                  props.dispatch({
+                    type: "GET_PAYOUTS",
+                    payload: res.data.payouts,
+                  });
+                });
+              }}
+            >
+              Next
+            </Button>
+          }
         </Col>
-      </Row> */}
+      </Row>
     </div>
   );
 };
--- src/services/DoctorService.js
+++ src/services/DoctorService.js
@@ -1,32 +1,37 @@
 import Axios from "../utils/axios";
 
 export async function getDoctors(page, category) {
-	let url = `/doctors?page=${page}`;
-	if (category) {
-		url = url + `&category=${category}`;
-	}
-	console.log("url", url);
-	const res = await Axios.get(url + "&is_verified=true");
-	let doctors = res.data.data.doctors;
-	const { data } = await Axios.get(`${url}&is_verified=false`);
-	doctors = [...doctors, ...data.data.doctors];
-	return doctors;
+  let url = `/doctors?page=${page}`;
+  if (category) {
+    url = url + `&category=${category}`;
+  }
+  console.log("url", url);
+  const res = await Axios.get(url + "&is_verified=true");
+  let doctors = res.data.data.doctors;
+  const { data } = await Axios.get(`${url}&is_verified=false`);
+  doctors = [...doctors, ...data.data.doctors];
+  return doctors;
 }
 
 export async function getDoctor(id) {
-	const { data } = await Axios.get(`/doctors/${id}`);
-	const doctor = data.data;
-	return doctor;
+  const { data } = await Axios.get(`/doctors/${id}`);
+  const doctor = data.data;
+  return doctor;
 }
 
 export async function setDoctor(id, status, key) {
-	let obj = {};
-	obj[key] = status;
-	const { data } = await Axios.patch(`/doctors/${id}`, obj);
-	return data;
+  let obj = {};
+  obj[key] = status;
+  const { data } = await Axios.patch(`/doctors/${id}`, obj);
+  return data;
 }
 
 export async function deleteDoctor(id) {
-	const { data } = await Axios.delete(`/doctors/${id}`);
-	return data;
+  const { data } = await Axios.delete(`/doctors/${id}`);
+  return data;
+}
+
+export async function getDoctorPayouts(id) {
+  const { data } = await Axios.get(`/doctors/payouts/${id}`);
+  return data;
 }
