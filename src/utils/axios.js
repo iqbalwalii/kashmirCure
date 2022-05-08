dc429362936368d59cd92fb160c1b47f9c4566b7
stupidlymoron <officialirfanafzal@gmail.com>
2022-05-08 22:23:40 +0530

    added access control header

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,11 +1,12 @@
 import Axios from "axios";
 const baseURL = process.env.NEXT_PUBLIC_API_URL;
 const axios = Axios.create({
-  baseURL,
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
+	baseURL,
+	headers: {
+		Accept: "application/json",
+		"Content-Type": "application/json",
+		" Access-Control-Allow-Origin": "*",
+	},
 });
 
 export default axios;
